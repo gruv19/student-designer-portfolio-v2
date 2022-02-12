7dafda4ed67a177cafe1734709daced3dd74a17d
gruv19 <grv1992@yandex.ru>
2022-02-12 22:47:22 +0500

    feat: Импортировал компонент 'Card'

--- src/components/Works.vue
+++ src/components/Works.vue
@@ -1,12 +1,18 @@
 <template>
   <main class="works">
+    <Filters />
+    <Card />
   </main>
 </template>
 
 <script>
+import Filters from './Filters.vue';
+import Card from './Card.vue';
 
 export default {
   name: 'Works',
-
+  components: {
+    Filters, Card,
+  },
 };
 </script>
