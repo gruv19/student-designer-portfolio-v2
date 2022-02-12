aa3a804d3c40e964a7dab4cecf2b4a10ab4edfa3
gruv19 <grv1992@yandex.ru>
2022-02-12 22:12:57 +0500

    feat: Создал базовую разметку компонента 'Contacts'

---
+++ src/components/Contacts.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="contacts">
+    <ul class="contacts__list">
+      <li class="contacts__item">
+        <a class="contacts__link" href="https://t.me/evg_GRU" target="_blank">Telegram</a>
+      </li>
+      <li class="contacts__item">
+        <a class="contacts__link" href="https://www.instagram.com/gru_jess/?utm_medium=copy_link" target="_blank">Instagram</a>
+      </li>
+      <li class="contacts__item">
+        <a class="contacts__link" href="mailto:evgeniya-malceva@bk.ru" target="_blank">E-mail</a>
+      </li>
+      <li class="contacts__item">
+        <a class="contacts__link" href="https://vk.com/ev.grusheva" target="_blank">Вконтакте</a>
+      </li>
+      <li class="contacts__item">
+        <a class="contacts__link" href="https://www.facebook.com/profile.php?id=100007436739623" target="_blank">Facebook</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Contacts',
+};
+</script>
