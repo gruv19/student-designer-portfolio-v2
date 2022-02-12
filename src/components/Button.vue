480286e069f2508e70db93ad58440db7a90d8609
gruv19 <grv1992@yandex.ru>
2022-02-12 23:18:41 +0500

    Добавил props для ссылки и текста кнопки

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,3 +1,19 @@
 <template>
-  <a class="button" href="#works">Учебные работы</a>
+  <a class="button" :href="link">{{ text }}</a>
 </template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    link: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
