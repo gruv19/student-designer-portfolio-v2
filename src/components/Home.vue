019e422b468e3a8855d0d4c1a8f8eaf4af70816e
gruv19 <grv1992@yandex.ru>
2022-02-12 22:15:42 +0500

    feat: Импортировал компоненты 'Button','Contacts','MainInfo'

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,20 @@
 <template>
   <header class="home">
+    <Contacts />
+    <MainInfo />
+    <Button />
   </header>
 </template>
 
 <script>
+import Contacts from './Contacts.vue';
+import MainInfo from './MainInfo.vue';
+import Button from './Button.vue';
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String,
+  components: {
+    Contacts, MainInfo, Button,
   },
 };
 </script>
