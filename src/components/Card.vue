a93b173c8df8f32a3972e2adcbd4dd8db042f016
gruv19 <grv1992@yandex.ru>
2022-02-12 22:46:41 +0500

    feat: Создал базовую разметку компонента 'Card'

---
+++ src/components/Card.vue
@@ -0,0 +1,24 @@
+<template>
+  <article class="card">
+      <img src="https://imgholder.ru/720x480/8270DE/F5F5F5.jpg&text=work_card&font=arial" alt="Изображение работы" class="card__image">
+      <h3 class="card__title">Slonum</h3>
+      <!-- <a href="/work_id" class="button">Посмотреть</a> -->
+      <Button />
+      <p class="card__subtitle">Тестовое задание</p>
+      <p class="card__task">
+        Задача: Landing page для участия в олимпиаде по математике, учиттывая ТЗ.
+      </p>
+      <a href="http://zhenyagru.tilda.ws/rynzhukfolio" class="card__link">Открыть в браузере</a>
+    </article>
+</template>
+
+<script>
+import Button from './Button.vue';
+
+export default {
+  name: 'Card',
+  components: {
+    Button,
+  },
+};
+</script>
