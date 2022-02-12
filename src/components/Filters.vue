3bd022f2a54c85fc7a4df5313f6034c4f7cbd55e
gruv19 <grv1992@yandex.ru>
2022-02-12 22:28:02 +0500

    feat: Создал базовую разметку компонента 'Filters'

---
+++ src/components/Filters.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="filters">
+    <ul class="filters__list">
+      <li class="filters__list">
+        <button class="filters__button">Все</button>
+      </li>
+      <li class="filters__list">
+        <button class="filters__button">Сайты</button>
+      </li>
+      <li class="filters__list">
+        <button class="filters__button">Мобильные приложения</button>
+      </li>
+    </ul>
+  </div>
+</template>
