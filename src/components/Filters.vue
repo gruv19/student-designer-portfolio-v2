3f4a0fbdd2bb901a8ac2d440d48deba4dfbace57
gruv19 <grv1992@yandex.ru>
2022-02-21 22:04:14 +0500

    feat: Реализовал основной функционал компонента

--- src/components/Filters.vue
+++ src/components/Filters.vue
@@ -1,15 +1,41 @@
 <template>
   <div class="filters">
     <ul class="filters__list">
-      <li class="filters__list">
-        <button class="filters__button">Все</button>
-      </li>
-      <li class="filters__list">
-        <button class="filters__button">Сайты</button>
-      </li>
-      <li class="filters__list">
-        <button class="filters__button">Мобильные приложения</button>
+      <li
+        class="filters__item"
+        v-for="type in types"
+        :key="type.title"
+      >
+        <button
+          class="filters__button"
+          :id="type.title"
+          :class="{ 'filters__button--active': activeFilter === type.title }"
+          @click.prevent="changeActiveFilter(type.title)"
+        >
+          {{ type.description }}
+        </button>
       </li>
     </ul>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'Filters',
+  data() {
+    return {
+      types: [
+        { title: 'all', description: 'Все' },
+        { title: 'website', description: 'Сайты' },
+        { title: 'mobile', description: 'Моб.приложения' },
+      ],
+      activeFilter: 'all',
+    };
+  },
+  methods: {
+    changeActiveFilter(filterTitle) {
+      this.activeFilter = filterTitle;
+    },
+  },
+};
+</script>
