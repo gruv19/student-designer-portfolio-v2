d79460dc009925dcef30b08af14ec0f4f84bf05d
gruv19 <grv1992@yandex.ru>
2022-03-05 21:25:03 +0500

    feat: Добавил action для взаимодействия с api, перенес activeFilter из компонента Filters

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,29 @@ import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    activeFilter: '',
   },
   mutations: {
+    setActiveFilter(state, filterName) {
+      state.activeFilter = filterName;
+    },
   },
   actions: {
+    async fetchWorkTypes() {
+      const data = await fetch('/api/getWorkTypes.php');
+      const result = await data.json();
+      return result;
+    },
+    async fetchWorks() {
+      const data = await fetch('/api/getWorks.php');
+      const result = await data.json();
+      return result;
+    },
+    async fetchWork(id) {
+      const data = await fetch(`/api/getWorkData.php?work_id=${id}`);
+      const result = await data.json();
+      return result;
+    },
   },
   modules: {
   },
