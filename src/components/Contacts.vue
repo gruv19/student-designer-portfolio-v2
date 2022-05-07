b23e9265de004e2b406002203d43eed8d5a3dac3
gruv19 <grv1992@yandex.ru>
2022-05-07 19:47:04 +0500

    refactor: Удалены ссылки на Instagram и Facebook

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -5,18 +5,18 @@
       <li class="contacts__item">
         <a class="contacts__link" href="https://t.me/evg_GRU" target="_blank">Telegram</a>
       </li>
-      <li class="contacts__item">
+      <!-- <li class="contacts__item">
         <a class="contacts__link" href="https://www.instagram.com/gru_jess/?utm_medium=copy_link" target="_blank">Instagram</a>
-      </li>
+      </li> -->
       <li class="contacts__item">
         <a class="contacts__link" href="mailto:evgeniya-malceva@bk.ru" target="_blank">E-mail</a>
       </li>
       <li class="contacts__item">
         <a class="contacts__link" href="https://vk.com/ev.grusheva" target="_blank">Вконтакте</a>
       </li>
-      <li class="contacts__item">
+      <!-- <li class="contacts__item">
         <a class="contacts__link" href="https://www.facebook.com/profile.php?id=100007436739623" target="_blank">Facebook</a>
-      </li>
+      </li> -->
     </ul>
     <div class="contacts__button">
       <Button link="#" text="Контакты" @click.prevent="showContactsHandler" />
