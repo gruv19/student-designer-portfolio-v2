134d03d8d112bdd7d15f505b73a53e57b86842ae
gruv19 <grv1992@yandex.ru>
2022-08-06 16:11:17 +0500

    refactoe: Перенес функцию загрузки всех работ в store

--- src/store/index.js
+++ src/store/index.js
@@ -57,6 +57,16 @@ export default createStore({
       const workImages = result.images ? JSON.parse(result.images) : [];
       commit('setWorkImages', workImages);
     },
+    async fetchWorks(context, queryParams) {
+      const { from = 0, filter = 'all', count = 0 } = queryParams;
+      let uri = `/api/getWorks.php?from=${from}&filter=${filter}`;
+      if (process.env.NODE_ENV === 'development') {
+        uri = `http://design-student-vue-2/api/getWorks.php?from=${from}&filter=${filter}&count=${count}`;
+      }
+      const data = await fetch(uri);
+      const result = await data.json();
+      return result;
+    },
     clearWorkImages({ commit }) {
       commit('setWorkImages', []);
     },
