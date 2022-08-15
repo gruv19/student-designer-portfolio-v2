3db3198563921a3382f94a5b1187c7623b4a81d0
gruv19 <grv1992@yandex.ru>
2022-08-15 12:50:42 +0500

    feat: Добавил страницу создания/редактирования работы с базовой разметкой

---
+++ src/views/CreateUpdateWork.vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="create-update-work">
+    <h1 class="create-update-work__title">Страница создания/редактирования работы</h1>
+    <div class="create-update-work__form">
+      <WorkForm :id="id" />
+    </div>
+  </main>
+</template>
+
+<script>
+import WorkForm from '@/components/WorkForm.vue';
+
+export default {
+  name: 'CreateUpdateWork',
+  components: { WorkForm },
+  props: {
+    id: {
+      required: false,
+    },
+  },
+};
+</script>
