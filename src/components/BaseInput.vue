aa8273df4931aa073f7b7aa70b8f2292534701f0
gruv19 <grv1992@yandex.ru>
2022-08-17 19:47:12 +0500

    feat: Добавил компонент BaseInput

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,44 @@
+<template>
+  <input
+    :type="type"
+    :name="name"
+    class="input"
+    :class="modifier ? `input--${modifier}` : ''"
+    @input="handleInput"
+    :placeholder="plcHolder"
+    :value="modelValue"
+  >
+</template>
+
+<script>
+export default {
+  name: 'BaseInput',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+      default: 'text',
+    },
+    modifier: {
+      type: String,
+      default: '',
+    },
+    modelValue: {
+      required: true,
+    },
+    plcHolder: {
+      required: true,
+    },
+  },
+  methods: {
+    handleInput(e) {
+      console.log(e.target.value);
+      this.$emit('update:modelValue', e.target.value);
+    },
+  },
+};
+</script>
