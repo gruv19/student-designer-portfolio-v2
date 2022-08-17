8131fd485e3380241065f84797f772528d2adf19
gruv19 <grv1992@yandex.ru>
2022-08-17 19:49:54 +0500

    style: Откорректировал с учетом изменений компонента Button и создания компонента BaseInput. Добавил стили странице.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,17 +1,28 @@
 <template>
   <div class="login">
+    <h1 class="login__title">Кабинет администратора</h1>
     <form action="" class="login__form" method="POST" @submit.prevent="login">
-      <input type="email" name="email" class="login__input" v-model.trim="email">
-      <input type="password" name="password" class="login__input" v-model="password">
-      <button type="submit" class="login__button">Войти</button>
+      <div class="login__input login__input--email">
+        <BaseInput type="email" name="email" v-model="email" plcHolder="Введи email" />
+      </div>
+      <div class="login__input login__input--password">
+        <BaseInput type="password" name="password" v-model="password" plcHolder="Введи пароль" />
+      </div>
+      <div class="login__buttons">
+        <Button text="Войти" modifier="invert" />
+        <Button text="Назад" modifier="cancel" type="reset" @click.prevent="goBack" />
+      </div>
     </form>
-      <button class="login__button" @click.prevent="isAuth">isAuth</button>
   </div>
 </template>
 
 <script>
+import Button from '@/components/Button.vue';
+import BaseInput from '@/components/BaseInput.vue';
+
 export default {
   name: 'Login',
+  components: { Button, BaseInput },
   data() {
     return {
       email: '',
@@ -20,9 +31,15 @@ export default {
   },
   methods: {
     async login() {
+      console.log(this.email, this.password);
       const answer = await this.$store.dispatch('auth', { email: this.email, password: this.password });
       this.$store.commit('setUserToken', { token: answer.data.users_token, expireDateToken: answer.data.users_token_expire_date });
       this.$router.push('/admin');
+    },
+    goBack() {
+      this.email = '';
+      this.password = '';
+      this.$router.go(-1);
     },
   },
 };
