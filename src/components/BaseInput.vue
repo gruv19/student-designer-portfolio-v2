b31856af121dda2f87800d8bcfa719f505a22c2f
gruv19 <grv1992@yandex.ru>
2022-08-24 13:01:26 +0500

    refactor: Удалил лошний console.log

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -36,7 +36,7 @@ export default {
   },
   methods: {
     handleInput(e) {
-      console.log(e.target.value);
+      // console.log(e.target.value);
       this.$emit('update:modelValue', e.target.value);
     },
   },
