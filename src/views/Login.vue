1d4363497335839a3faea38e803997340362a9cf
gruv19 <grv1992@yandex.ru>
2022-08-29 00:04:39 +0500

    refactor: Удалил лишний console.log

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -31,7 +31,6 @@ export default {
   },
   methods: {
     async login() {
-      console.log(this.email, this.password);
       const answer = await this.$store.dispatch('auth', { email: this.email, password: this.password });
       this.$store.commit('setUserToken', { token: answer.data.users_token, expireDateToken: answer.data.users_token_expire_date });
       this.$router.push('/admin');
