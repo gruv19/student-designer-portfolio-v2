cd08038c677cff04de345c9556a4b7252bb58043
gruv19 <grv1992@yandex.ru>
2022-08-29 12:48:24 +0500

    feat: Добавил компонент InputErrors

---
+++ src/components/InputErrors.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="input-errors">
+    <span class="input-errors__error" v-for="e in errors" :key="e.$uid">
+      {{ e.$message }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputErrors',
+  props: {
+    errors: {
+      required: true,
+    },
+  },
+};
+</script>
