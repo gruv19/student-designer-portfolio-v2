41353871f577f52c06266092f7ad67da30cb1eef
gruv19 <grv1992@yandex.ru>
2022-09-05 22:57:17 +0500

    feat: Установил и добавил в проект '@kyvg/vue3-notification' для вывода сообщений об ошибках

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       <component :is="Component" />
     </keep-alive>
   </router-view>
+  <notifications position="bottom right" />
 </template>
 <style lang="scss">
 @import '@/style/style.scss';
--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import Notifications from '@kyvg/vue3-notification'; // eslint-disable-line
 
-createApp(App).use(store).use(router).mount('#app');
+createApp(App).use(store).use(router).use(Notifications).mount('#app'); // eslint-disable-line
