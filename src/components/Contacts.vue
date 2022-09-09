ff1f8fa84a742fdd66813c766ed785310099b17d
gruv19 <grv1992@yandex.ru>
2022-09-09 20:41:33 +0500

    refactor: Реализовал получение списка контактных данных с сервера

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -1,47 +1,31 @@
 <template>
   <div class="contacts">
-    <div class="contacts__overlay" @click.prevent="hideContactsHandler" v-if="menuActivator"></div>
-    <ul class="contacts__list" :class="menuActivator ? 'contacts__list--active' : ''">
-      <li class="contacts__item">
-        <a class="contacts__link" href="https://t.me/evg_GRU" target="_blank">Telegram</a>
+    <ul class="contacts__list">
+      <li class="contacts__item" v-for="contact in contacts" :key="contact.id">
+        <a class="contacts__link" :href="contact.link" target="_blank">{{ contact.title }}</a>
       </li>
-      <!-- <li class="contacts__item">
-        <a class="contacts__link" href="https://www.instagram.com/gru_jess/?utm_medium=copy_link" target="_blank">Instagram</a>
-      </li> -->
-      <li class="contacts__item">
-        <a class="contacts__link" href="mailto:evgeniya-malceva@bk.ru" target="_blank">E-mail</a>
-      </li>
-      <li class="contacts__item">
-        <a class="contacts__link" href="https://vk.com/ev.grusheva" target="_blank">Вконтакте</a>
-      </li>
-      <!-- <li class="contacts__item">
-        <a class="contacts__link" href="https://www.facebook.com/profile.php?id=100007436739623" target="_blank">Facebook</a>
-      </li> -->
     </ul>
-    <!-- <div class="contacts__button">
-      <Button link="#" text="Контакты" @click.prevent="showContactsHandler" />
-    </div> -->
   </div>
 </template>
 
 <script>
-// import Button from '@/components/Button.vue';
+import { ref, onMounted } from 'vue';
+import store from '@/store/index';
 
 export default {
   name: 'Contacts',
-  // components: { Button },
-  data() {
-    return {
-      menuActivator: false,
-    };
-  },
-  methods: {
-    showContactsHandler() {
-      this.menuActivator = true;
-    },
-    hideContactsHandler() {
-      this.menuActivator = false;
-    },
+  setup() {
+    const contacts = ref({});
+
+    onMounted(async () => {
+      try {
+        contacts.value = await store.dispatch('readContacts');
+      } catch (error) {
+        store.dispatch('showError', error.message);
+      }
+    });
+
+    return { contacts };
   },
 };
 </script>
