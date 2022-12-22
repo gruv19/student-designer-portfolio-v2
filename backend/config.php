d1e9846863d38d863f8926f6b680c7f2162ace11
gruv19 <grv1992@yandex.ru>
2022-12-22 17:31:06 +0000

    feat: Добавил файл с константами для подключения к БД

---
+++ backend/config.php
@@ -0,0 +1,7 @@
+<?php
+define( 'DBSERVER', 'database' );
+define( 'DBNAME', getenv('MYSQL_DATABASE') );
+define( 'DBUSER', getenv('MYSQL_USER') );
+define( 'DBPASSWORD', getenv('MYSQL_PASSWORD') );
+
+?>
