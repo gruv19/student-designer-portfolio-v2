f3f1d8b2edfdecd9e052ac0a8608971bd58ab9fa
gruv19 <grv1992@yandex.ru>
2022-12-22 17:47:37 +0000

    feat: Добавил в скрипт создание таблицы work_types

--- database/update_user.sh
+++ database/update_user.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
 mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "ALTER USER ${MYSQL_USER}@'%' IDENTIFIED WITH mysql_native_password BY '${MYSQL_PASSWORD}';"
+
+# create table work_types
+mysql -u root -p${MYSQL_ROOT_PASSWORD} <<EOF
+  use ${MYSQL_DATABASE};
+  DROP TABLE IF EXISTS `work_types`;
+  CREATE TABLE `work_types` (
+    `work_types_id` int(11) NOT NULL,
+    `work_types_title` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+    `work_types_description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  INSERT INTO `work_types` (`work_types_id`, `work_types_title`, `work_types_description`) VALUES
+  (1325, 'website', 'Сайты'),
+  (1326, 'mobile', 'Мобильные приложения');
+
+  ALTER TABLE `work_types`
+    ADD PRIMARY KEY (`work_types_id`);
+
+  ALTER TABLE `work_types`
+    MODIFY `work_types_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1362;
+  COMMIT;
+EOF
