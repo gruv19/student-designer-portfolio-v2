470884f0e96229c22082936f2c5c94ed43a6e835
gruv19 <grv1992@yandex.ru>
2023-01-04 07:39:15 +0000

    refactor: переименовал файл, добавил обработку OPTIONS запроса, убрал использование cookie

--- backend/user_logout.php
+++ backend/user_logout.php
@@ -1,16 +1,24 @@
 <?php
-  header('Access-Control-Allow-Origin: *');
+  require_once('./config.php');
+  require_once('./utils.php');
+
+  header('Access-Control-Allow-Origin: ' . FRONTEND_HOST);
   header('Access-Control-Allow-Methods: GET, POST');
   header('Access-Control-Allow-Headers: Content-Type');
   header('Content-Type: application/json');
 
-  require_once('./config.php');
-  require_once('./utils.php');
+  if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    return "ok";
+  }
 
   $mysqli = db_connect(DBSERVER, DBUSER, DBPASSWORD, DBNAME);
 
-  $token = $mysqli->real_escape_string($_COOKIE['token']);
-  $is_auth = isAuth($mysqli, $token);
+  $_POST = json_decode(file_get_contents("php://input"), true);
+
+  $token = $mysqli->real_escape_string($_POST['token']);
+
+  $is_auth = is_auth($mysqli, $token);
+
   if (!$is_auth['data']['isAuth']) {
     http_response_code(403);
     $answer = array('status' => 'error', 'message' => 'Not loggeed!');
@@ -27,6 +35,5 @@
   }
 
   $answer = array('status' => 'succes', 'data' => ['logout' => true]);
-  setcookie('token', '', time() - 3600);
   echo json_encode($answer);
 ?>
