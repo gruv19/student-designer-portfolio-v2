053d94021dee254ef09a599b9fd64380a96508d8
gruv19 <grv1992@yandex.ru>
2023-01-04 07:40:15 +0000

    feat: Добавил переменные FRONTEND_HOST и SAULT

--- backend/config.php
+++ backend/config.php
@@ -3,5 +3,6 @@ define( 'DBSERVER', 'database' );
 define( 'DBNAME', getenv('MYSQL_DATABASE') );
 define( 'DBUSER', getenv('MYSQL_USER') );
 define( 'DBPASSWORD', getenv('MYSQL_PASSWORD') );
-
+define('SAULT', getenv('SAULT'));
+define('FRONTEND_HOST', getenv('FRONTEND_HOST'));
 ?>
