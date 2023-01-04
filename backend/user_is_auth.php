6734583ac384123dabf9948332156950d47c362f
gruv19 <grv1992@yandex.ru>
2023-01-04 07:38:18 +0000

    refactor: переименовал файл в едином стиле, убрал использование cookie

--- backend/user_is_auth.php
+++ backend/user_is_auth.php
@@ -1,22 +1,19 @@
 <?php
-  header('Access-Control-Allow-Origin: *');
+  require_once('./config.php');
+  require_once('./utils.php');
+  
+  header('Access-Control-Allow-Origin: ' . FRONTEND_HOST);
   header('Access-Control-Allow-Methods: GET, POST');
   header('Access-Control-Allow-Headers: Content-Type');
   header('Content-Type: application/json');
-
-  require_once('./config.php');
-  require_once('./utils.php');
 
   $mysqli = db_connect(DBSERVER, DBUSER, DBPASSWORD, DBNAME);
 
   $_POST = json_decode(file_get_contents("php://input"), true);
 
   $token = $mysqli->real_escape_string($_POST['token']);
-  if ($token === '') {
-    $token = $_COOKIE['token'];
-  }
 
-  $answer = isAuth($mysqli, $token);
+  $answer = is_auth($mysqli, $token);
 
   echo json_encode($answer);
 ?>
