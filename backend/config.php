c371bcd54667fc15c42f7f98ef784b44650ee59a
gruv19 <grv1992@yandex.ru>
2023-01-05 16:21:34 +0000

    feat: Добавил константу BACKEND_HOST

--- backend/config.php
+++ backend/config.php
@@ -5,4 +5,5 @@ define( 'DBUSER', getenv('MYSQL_USER') );
 define( 'DBPASSWORD', getenv('MYSQL_PASSWORD') );
 define('SAULT', getenv('SAULT'));
 define('FRONTEND_HOST', getenv('FRONTEND_HOST'));
+define('BACKEND_HOST', getenv('BACKEND_HOST'));
 ?>
