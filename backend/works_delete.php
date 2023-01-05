4a064a0694661aeff1b9d713c951bc13dfc5e571
gruv19 <grv1992@yandex.ru>
2023-01-05 13:11:14 +0000

    refactor: Переименовал файл к единому стилю, убрал использование cookie файлов

--- backend/works_delete.php
+++ backend/works_delete.php
@@ -1,24 +1,29 @@
 <?php
-  header('Access-Control-Allow-Origin: *');
+  require_once('./config.php');
+  require_once('./utils.php');
+
+  header('Access-Control-Allow-Origin: ' . FRONTEND_HOST);
   header('Access-Control-Allow-Methods: GET, POST');
   header('Access-Control-Allow-Headers: Content-Type');
   header('Content-Type: application/json');
 
-  require_once('./config.php');
-  require_once('./utils.php');
+  if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    return "ok";
+  }
 
   $mysqli = db_connect(DBSERVER, DBUSER, DBPASSWORD, DBNAME);
 
-  $token = $_COOKIE['token'];
-  $is_auth = isAuth($mysqli, $token);
+  $_POST = json_decode(file_get_contents("php://input"), true);
+  $token = $mysqli->real_escape_string($_POST['token']);
+  $work_id = $mysqli->real_escape_string($_POST['id']);
+  
+  $is_auth = is_auth($mysqli, $token);
   if (!$is_auth['data']['isAuth']) {
     http_response_code(403);
     $answer = array('status' => 'error', 'message' => 'Access is denied!');
     die(json_encode($answer));
   }
 
-  $_POST = json_decode(file_get_contents("php://input"), true);
-  $work_id = $mysqli->real_escape_string($_POST['id']);
   if ($work_id) {
     remove_main_image($mysqli, $work_id);
     remove_work_images($mysqli, $work_id);
